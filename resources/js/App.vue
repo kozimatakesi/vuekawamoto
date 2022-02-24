c2cbe5abaf83c53934115a6e406fe1eb40968ef4
kozimatakesi <riverbook708@gmail.com>
2022-02-24 14:12:16 +0000

    section 8 half

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -12,6 +12,7 @@
   </div>
 </template>
 <script>
+import { INTERNAL_SERVER_ERROR } from './util'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 
@@ -19,6 +20,24 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  computed: {
+      errorCode() {
+          return this.$store.state.error.code
+      }
+  },
+  watch: {
+      errorCode: {
+          handler(val) {
+              if(val === INTERNAL_SERVER_ERROR) {
+                  this.$router.push('/500')
+              }
+          },
+          immediate: true
+      },
+      $route() {
+          this.$store.commit('error/setCode', null)
+      }
   }
 }
 </script>
--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -64,15 +64,21 @@ export default {
   },
   methods: {
     async login () {
-    // authストアのloginアクションを呼び出す
-    await this.$store.dispatch('auth/login', this.loginForm)
+        // authストアのloginアクションを呼び出す
+        await this.$store.dispatch('auth/login', this.loginForm)
 
-    // トップページに移動する
-    this.$router.push('/')
+        if(this.apiStatus) {
+            this.$router.push('/')
+        }
     },
      async register() {
           await this.$store.dispatch('auth/register', this.registerForm)
           this.$router.push('/')
+      }
+  },
+  computed: {
+      apiStatus() {
+          return this.$store.state.auth.apiStatus
       }
   }
 }
